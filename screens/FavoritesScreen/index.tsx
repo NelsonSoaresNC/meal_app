7b656f9015075b10e898debf4d58a957740d1b96
Nelson de Sousa Soares <nelson.soares@netcracker.com>
2025-08-08 13:31:54 -0300

    useContext to favorite meals ok

--- screens/FavoritesScreen/index.tsx
+++ screens/FavoritesScreen/index.tsx
@@ -1,8 +1,25 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import MealsList from "../../components/MealsList";
+import { MEALS } from "../../data/dummy-data";
+import { FavoritesContext } from "../../store/context/favorites-contex";
+import { Text, View } from "react-native";
 import { styles } from "./styles";
 
 export default function FavoritesScreen(){
+    
+    const favoriteMealsCtx = useContext(FavoritesContext);
+
+    const favoritesMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id))
+
+    if(favoritesMeals.length === 0){
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>You hava no favorite meals yet</Text>
+            </View>
+        );
+    }
+
     return (
-        <></>
+       <MealsList items={favoritesMeals} />
     );
 }
